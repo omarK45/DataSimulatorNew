5ee8d287da7502d153712f67ae191c89adc57c57
omar ahmed mohmed refaat <119802537+omarrrefaatt@users.noreply.github.com>
2023-04-18 07:04:58 +0200

    Add files via upload

---
+++ UI.cpp
@@ -0,0 +1,24 @@
+#include "UI.h"
+void UI::print(Processor *p[], int numprocessor, Queue<Process>*blk, Process*run[], PriorityQueue<Process>*trm,int timestep) {
+cout<<"current Timestep:"<<timestep<<endl;
+cout<<"-----------------   RDY processes __________________________"<<endl;
+for(int i=0;i<numprocessor;i++){
+    cout<<"processor "<<i+1<<"  ["<<p[i]->return_Type()<<"]"<<": "<<"RDY";
+    p[i]->printRDY();
+    cout<<endl;
+}
+    cout<<"-----------------   BLK processes __________________________"<<endl;
+cout<<blk->getcount()<<" BLK: ";
+blk->printQueue();
+cout<<endl;
+cout<<"-----------------   RUN processes __________________________"<<endl;
+cout<<numprocessor<<" RUN: ";
+    for (int i = 0; i < numprocessor; ++i) {
+cout<<p[i]<<"(p"<<i+1<<"),  ";
+    }
+cout<<endl;
+cout<<"-----------------   TRM processes __________________________"<<endl;
+cout<<trm->count<<" BLK: ";
+trm->printQueue();
+cout<<endl;
+}
